2a2d0bb6762a488732a7d9234b73dc503436d520
gustavosegantini <70765859+gustavosegantini@users.noreply.github.com>
2023-06-20 01:10:16 -0300

    Script campos dinamicos

--- form.php
+++ form.php
@@ -9,13 +9,37 @@
         <input type="text" name="nome" placeholder="Nome" required>
         <input type="text" name="cargo" placeholder="Cargo" required>
         <textarea name="resumo" placeholder="Resumo" required></textarea>
-        <input type="text" name="empresa" placeholder="Empresa" required>
-        <input type="date" name="data_inicio" placeholder="Data de Início" required>
-        <input type="date" name="data_fim" placeholder="Data de Término">
-        <input type="text" name="titulo_cargo" placeholder="Título do Cargo" required>
-        <textarea name="descricao_cargo" placeholder="Descrição do Cargo" required></textarea>
-        <input type="checkbox" name="emprego_atual" value="1"> Emprego Atual
+
+        <div id="experiencias">
+            <div class="experiencia">
+                <input type="text" name="empresa[]" placeholder="Empresa" required>
+                <input type="date" name="data_inicio[]" placeholder="Data de Início" required>
+                <input type="date" name="data_fim[]" placeholder="Data de Término">
+                <input type="text" name="titulo_cargo[]" placeholder="Título do Cargo" required>
+                <textarea name="descricao_cargo[]" placeholder="Descrição do Cargo" required></textarea>
+                <input type="checkbox" name="emprego_atual[]" value="1"> Emprego Atual
+            </div>
+        </div>
+
+        <button type="button" id="add">Adicionar Cargo</button>
         <button type="submit">Enviar</button>
     </form>
+
+    <script>
+        document.getElementById('add').addEventListener('click', function() {
+            var experiencias = document.getElementById('experiencias');
+            var novaExperiencia = document.createElement('div');
+            novaExperiencia.className = 'experiencia';
+            novaExperiencia.innerHTML = `
+                <input type="text" name="empresa[]" placeholder="Empresa" required>
+                <input type="date" name="data_inicio[]" placeholder="Data de Início" required>
+                <input type="date" name="data_fim[]" placeholder="Data de Término">
+                <input type="text" name="titulo_cargo[]" placeholder="Título do Cargo" required>
+                <textarea name="descricao_cargo[]" placeholder="Descrição do Cargo" required></textarea>
+                <input type="checkbox" name="emprego_atual[]" value="1"> Emprego Atual
+            `;
+            experiencias.appendChild(novaExperiencia);
+        });
+    </script>
 </body>
 </html>
--- submit.php
+++ submit.php
@@ -1,21 +1,26 @@
 <?php
 require_once('../db_connection.php'); // assumindo que db_connection.php está na pasta anterior
 
-// Preparar e vincular
-$stmt = $conn->prepare("INSERT INTO Curriculos (nome, cargo, resumo, empresa, data_inicio, data_fim, titulo_cargo, descricao_cargo, emprego_atual) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
-$stmt->bind_param("ssssssssi", $nome, $cargo, $resumo, $empresa, $data_inicio, $data_fim, $titulo_cargo, $descricao_cargo, $emprego_atual);
-
-// Definir parâmetros e executar
 $nome = $_POST['nome'];
 $cargo = $_POST['cargo'];
 $resumo = $_POST['resumo'];
-$empresa = $_POST['empresa'];
-$data_inicio = $_POST['data_inicio'];
-$data_fim = $_POST['data_fim'];
-$titulo_cargo = $_POST['titulo_cargo'];
-$descricao_cargo = $_POST['descricao_cargo'];
-$emprego_atual = isset($_POST['emprego_atual']) ? 1 : 0;
-$stmt->execute();
+
+// Preparar a declaração SQL
+$stmt = $conn->prepare("INSERT INTO Curriculos (nome, cargo, resumo, empresa, data_inicio, data_fim, titulo_cargo, descricao_cargo, emprego_atual) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)");
+$stmt->bind_param("ssssssssi", $nome, $cargo, $resumo, $empresa, $data_inicio, $data_fim, $titulo_cargo, $descricao_cargo, $emprego_atual);
+
+// Iterar por cada empresa preenchida
+for ($i = 0; $i < count($_POST['empresa']); $i++) {
+    $empresa = $_POST['empresa'][$i];
+    $data_inicio = $_POST['data_inicio'][$i];
+    $data_fim = $_POST['data_fim'][$i];
+    $titulo_cargo = $_POST['titulo_cargo'][$i];
+    $descricao_cargo = $_POST['descricao_cargo'][$i];
+    $emprego_atual = isset($_POST['emprego_atual'][$i]) ? 1 : 0;
+
+    // Executar a declaração SQL
+    $stmt->execute();
+}
 
 echo "Currículo enviado com sucesso!";
 
